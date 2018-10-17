d5391ac4e9d0e7d91c8f14b0e2fcfbc9afbe4af6
Guilherme Galarça Dutra <udutra@gmail.com>
2018-10-17 15:41:03 -0300

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	private Rigidbody rb;
+	public LayerMask ground;
+	public float jumpForce = 10f;
+	
+	private void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	private void OnTriggerEnter(Collider other){
+		if(((1 << other.gameObject.layer) & ground) == 0)
+		{
+			return;
+		}
+		
+		rb.velocity = Vector3.zero;
+		rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+	}
+}
---
+++ Assets/Scripts/BaseControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseControl : MonoBehaviour {
+
+    public float speed = 200f;
+
+    private void Update()
+    {
+        Rotate();
+    }
+
+    public void Rotate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X") * -1, 0) * speed * Time.deltaTime);
+        }
+    }
+}
